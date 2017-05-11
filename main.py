1ef13f302537d60a225451e255c1a552b3b1ffb4
CPritchard <pritchardc2015@gmail.com>
2017-05-11 12:20:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,10 @@
 #Author: Callum Pritchard, Joachim Hummel
 #Project Name: Scroll-Phat
 #Project Description: Getting data from MQTT and displaying it on the scroll-phat
-#Version Number: 0.4
+#Version Number: 0.5
 #Date: 10/5/17
-#Release State: Alpha testing
-#Changes: Changed from infinite too to looping after displaying, it means it can't fully
-#               accept an input until it's displayed the last
+#Release State: Beta testing
+#Changes: Changed the replace to a range to output as to avoide replacing the letter B
 
 #needed commands
 #pip3 install paho-mqtt
@@ -16,7 +15,7 @@
 
 import paho.mqtt.client as mqtt  
 import time
-import scrollphat
+#import scrollphat
 
 global output
 output = ''
@@ -26,15 +25,16 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('#')
 
 def on_message(client, userdata, msg):  #triggers on an update
-    scrollphat.clear()
+    #scrollphat.clear()
     global output
     if  str(msg.topic) == 'msg/sensor1':  #name of the publisher
-        print(str(msg.payload).replace('b', '').replace("'", ""))
-        output = str(msg.payload).replace('b', '').replace("'", "") + '   '
+        print(str(msg.payload)[2:len(str(msg.payload))-2])
+        output = str(msg.payload)[2:len(str(msg.payload))-2] + '   '
         #since the loop cannot be here, the text needs to looped in itself so it can be displayed
         #until a new value is sent via mqtt
-
-scrollphat.set_brightness(2)
+        
+#scrollphat.set_rotate(True)
+#scrollphat.set_brightness(2)
 
 client = mqtt.Client()
 client.connect("mqtt.unixweb.de",1883,60)  #connects to the broker
@@ -43,9 +43,9 @@ client.on_message = on_message
 
 while True:
     client.loop()        
-    scrollphat.write_string(output, 11)  #writes to display starting at position 11 on x axis
+    #scrollphat.write_string(output, 11)  #writes to display starting at position 11 on x axis
     for i in range(len(output) + 3):
-        scrollphat.scroll() #initiates the scrolling
+        #scrollphat.scroll() #initiates the scrolling
         time.sleep(0.1)    
     
     
