d1c5e284b41383ae0d7ae9c053565c2d48e28a92
CPritchard <pritchardc2015@gmail.com>
2017-05-11 12:26:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 
 import paho.mqtt.client as mqtt  
 import time
-#import scrollphat
+import scrollphat
 
 global output
 output = ''
@@ -25,16 +25,16 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('#')
 
 def on_message(client, userdata, msg):  #triggers on an update
-    #scrollphat.clear()
+    scrollphat.clear()
     global output
     if  str(msg.topic) == 'msg/sensor1':  #name of the publisher
-        print(str(msg.payload)[2:len(str(msg.payload))-2])
-        output = str(msg.payload)[2:len(str(msg.payload))-2] + '   '
+        print(str(msg.payload)[2:len(str(msg.payload))-1])
+        output = str(msg.payload)[2:len(str(msg.payload))-1] + '   '
         #since the loop cannot be here, the text needs to looped in itself so it can be displayed
         #until a new value is sent via mqtt
         
-#scrollphat.set_rotate(True)
-#scrollphat.set_brightness(2)
+scrollphat.set_rotate(True)
+scrollphat.set_brightness(2)
 
 client = mqtt.Client()
 client.connect("mqtt.unixweb.de",1883,60)  #connects to the broker
@@ -43,9 +43,9 @@ client.on_message = on_message
 
 while True:
     client.loop()        
-    #scrollphat.write_string(output, 11)  #writes to display starting at position 11 on x axis
+    scrollphat.write_string(output, 11)  #writes to display starting at position 11 on x axis
     for i in range(len(output) + 3):
-        #scrollphat.scroll() #initiates the scrolling
+        scrollphat.scroll() #initiates the scrolling
         time.sleep(0.1)    
     
     
